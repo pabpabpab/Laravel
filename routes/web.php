345a14777e944f6784d40603c169855951301b89
pabpabpab <urallove@yandex.ru>
2020-12-09 20:56:36 +0500

    hw1 done

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return view('hello', ['routeName' => 'hello']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['routeName' => 'about']);
+});
+
+Route::get('/news', function () {
+    return view('news', ['routeName' => 'news']);
 });
