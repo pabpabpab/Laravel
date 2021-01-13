0aaa6ca14bb01ac090bffe70f05a6881503bdf58
pabpabpab <55209398+pabpabpab@users.noreply.github.com>
2021-01-14 03:35:43 +0500

    Delete menu.blade.php

--- resources/views/blocks/menu.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="menu">
-    @foreach($menu as $item)
-        <a href="{{route($item['alias'])}}" class="header_link">
-            {{$item['title']}}
-        </a>
-    @endforeach
-</div>
